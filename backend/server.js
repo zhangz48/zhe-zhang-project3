7823851eda1081f8a53d735473c73fd051371696
Zack Zhang <realzhezhang@gmail.com>
2024-12-10 01:56:40 -0800

    2 - auth

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 
