cbc7a0c8b45db6055158ba2383cb89115a0a4bd4
Zack Zhang <realzhezhang@gmail.com>
2024-12-10 18:22:03 -0800

    8 - working auth

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001",
+        changeOrigin: true,
+      },
+    },
   },
 });
