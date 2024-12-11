f66808d64d80ff96434a79070bea31442de9a40a
Zack Zhang <realzhezhang@gmail.com>
2024-12-10 17:16:35 -0800

    6 - UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
+			{/* Common component, bc it's not wrapped with Routes */}
+			<Sidebar />
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/signup' element={<SignUpPage />} />
 				<Route path='/login' element={<LoginPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
 			</Routes>
 		</div>
 	);
---
+++ frontend/src/components/common/Post.jsx
@@ -0,0 +1,52 @@
+import { FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Post = ({ post }) => {
+    const postOwner = post.user;
+
+    const isMyPost = true;
+
+    const formattedDate = "1h";
+
+    const handleDeletePost = () => { };
+
+    return (
+        <>
+            <div className='flex gap-2 items-start p-4 border-b border-gray-700'>
+                <div className='avatar'>
+                    <Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full overflow-hidden'>
+                        <img src={postOwner.profileImg || "/avatar-placeholder.png"} />
+                    </Link>
+                </div>
+                <div className='flex flex-col flex-1'>
+                    <div className='flex gap-2 items-center'>
+                        <Link to={`/profile/${postOwner.username}`} className='font-bold'>
+                            {postOwner.fullName}
+                        </Link>
+                        <span className='text-gray-700 flex gap-1 text-sm'>
+                            <Link to={`/profile/${postOwner.username}`}>@{postOwner.username}</Link>
+                            <span>·</span>
+                            <span>{formattedDate}</span>
+                        </span>
+                        {isMyPost && (
+                            <span className='flex justify-end flex-1'>
+                                <FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+                            </span>
+                        )}
+                    </div>
+                    <div className='flex flex-col gap-3 overflow-hidden'>
+                        <span>{post.text}</span>
+                        {post.img && (
+                            <img
+                                src={post.img}
+                                className='h-80 object-contain rounded-lg border border-gray-700'
+                                alt=''
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+export default Post;
