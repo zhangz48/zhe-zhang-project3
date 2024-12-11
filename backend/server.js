e275ac8c5d313a1c2eb4f547632878b282655824
Zack Zhang <realzhezhang@gmail.com>
2024-12-10 18:21:43 -0800

    8 - working auth

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ cloudinary.config({
 });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
